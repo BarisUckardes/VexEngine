69602124c46e1b1cd213dcf51ebf89f6e8e5f94b
BarisUckardes <68399075+BarisUckardes@users.noreply.github.com>
2021-12-21 16:57:15 +0300

    Compute shaders created

--- Vex/Compute/ComputeShader.cs
+++ Vex/Compute/ComputeShader.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vex.Framework;
-
+using OpenTK.Graphics.OpenGL4;
 namespace Vex.Compute
 {
+    /// <summary>
+    /// A shader specialized in compute shading
+    /// </summary>
     public sealed class ComputeShader : AssetObject
     {
         public ComputeShader()
@@ -14,6 +17,9 @@ namespace Vex.Compute
 
         }
 
+        /// <summary>
+        /// Returns the last compilation message (if any)
+        /// </summary>
         public string LastCompileErrorMessage
         {
             get
@@ -32,16 +38,55 @@ namespace Vex.Compute
             /*
              * Compile
              */
+            Invalidate();
+            
         }
-        public void Dispatch(int handle,string entryPoint,int threadX,int threadY,int threadZ)
+        public void DispatchAndWait(int threadX,int threadY,int threadZ)
         {
             /*
-             * Start dispatch
+             * Use compute shader program
              */
+            GL.UseProgram(m_ProgramHandle);
 
             /*
-             * Wait for it to finish
+             * Create fence
              */
+            IntPtr fencePtr = GL.FenceSync(SyncCondition.SyncGpuCommandsComplete, WaitSyncFlags.None);
+
+            /*
+             * Dispatch compute shader
+             */
+            GL.DispatchCompute(threadX, threadY, threadZ);
+
+            /*
+             * Wait for fence
+             */
+            GL.WaitSync(fencePtr, WaitSyncFlags.None, 0);
+
+            /*
+             * Delete fence
+             */
+            GL.DeleteSync(fencePtr);
+        }
+
+        public ComputeWaitHandle Dispatch(int threadX,int threadY,int threadZ)
+        {
+            /*
+            * Use compute shader program
+            */
+            GL.UseProgram(m_ProgramHandle);
+
+            /*
+             * Create fence
+             */
+            IntPtr fencePtr = GL.FenceSync(SyncCondition.SyncGpuCommandsComplete, WaitSyncFlags.None);
+
+            /*
+             * Dispatch compute shader
+             */
+            GL.DispatchCompute(threadX, threadY, threadZ);
+
+            return new ComputeWaitHandle(fencePtr);
         }
         public void SetTexture3D()
         {
@@ -58,10 +103,51 @@ namespace Vex.Compute
 
         public override void Destroy()
         {
-            throw new NotImplementedException();
+
+        }
+
+        private void Invalidate()
+        {
+            /*
+             * Create shader id
+             */
+            int shaderID = GL.CreateShader(ShaderType.ComputeShader);
+
+            /*
+             * Upload shader source
+             */
+            GL.ShaderSource(shaderID,m_Source);
+
+            /*
+             * Compile shader
+             */
+            GL.CompileShader(shaderID);
+
+            /*
+             * Create shader program id
+             */
+            int shaderProgram = GL.CreateProgram();
+
+            /*
+             * Attach shader
+             */
+            GL.AttachShader(shaderProgram, shaderID);
+
+            /*
+             * Link program
+             */
+            GL.LinkProgram(shaderProgram);
+
+            /*
+             * Set handles
+             */
+            m_ProgramHandle = shaderProgram;
+            m_ShaderHandle = shaderID;
         }
 
         private string m_Source;
+        private int m_ProgramHandle;
+        private int m_ShaderHandle;
         private string m_LastCompileErrorMessage;
     }
 }
---
+++ Vex/Compute/ComputeWaitHandle.cs
@@ -0,0 +1,44 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vex.Compute
+{
+    /// <summary>
+    /// And wait handle object for compute shaders
+    /// </summary>
+    public sealed class ComputeWaitHandle
+    {
+        internal ComputeWaitHandle(in IntPtr fencePtr)
+        {
+            m_FencePtr = fencePtr;
+        }
+
+        public void WaitForFinish()
+        {
+            /*
+            * Wait for fence
+            */
+            GL.WaitSync(m_FencePtr, WaitSyncFlags.None, 0);
+
+            /*
+             * Delete fence
+             */
+            GL.DeleteSync(m_FencePtr);
+        }
+        /// <summary>
+        /// Returns the fence ptr
+        /// </summary>
+        internal IntPtr FencePtr
+        {
+            get
+            {
+                return m_FencePtr;
+            }
+        }
+        private IntPtr m_FencePtr;
+    }
+}
